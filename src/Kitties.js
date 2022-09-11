57212b24edddea87188308fe9bc0bcb28d3bd579
memorycancel <memorycancel@gmail.com>
2022-09-12 01:32:08 +0800

    Update KittyCnt state

--- src/Kitties.js
+++ src/Kitties.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, Grid } from 'semantic-ui-react'
 
 import { TxButton } from './substrate-lib/components'
+import { useSubstrateState } from './substrate-lib'
 
 export default function Main (props) {
+  const { api } = useSubstrateState()
 
-  const [kittyCnt] = useState(0)
+  const [kittyCnt, setKittyCnt] = useState(0)
 
   const [status] = useState('')
+
+  useEffect(() => {
+    let unsubscribe
+    api.query.kitties
+      .nextKittyId(newValue => {
+        // The storage value is an Option<u32>
+        // So we have to check whether it is None first
+        // There is also unwrapOr
+        console.log("=============")
+        setKittyCnt(newValue.toNumber())
+      })
+      .then(unsub => {
+        unsubscribe = unsub
+      })
+      .catch(console.error)
+
+    return () => unsubscribe && unsubscribe()
+  }, [api.query.kitties])
 
   return <Grid.Column width={16}>
     <h1>小毛孩</h1>
